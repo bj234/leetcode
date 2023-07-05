56a4bf707653018cc0d10d6a87166404ebde4bbc
bj234 <40837834+bj234@users.noreply.github.com>
2023-07-05 22:26:31 +0530

    Create 1493. Longest Subarray of 1's After Deleting One Element.py
    Binary search

---
+++ 1493. Longest Subarray of 1's After Deleting One Element.py
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool ispossible(vector<int>& nums,int mid){
+        int l=0,r=mid,count1=0;
+        for(int i=0;i<=r;i++){
+            if(nums[i]==1) count1++;
+        }
+        if(count1>=mid) return true;
+        while(r<nums.size()-1){
+            if(nums[l]==1) count1-=1;
+            l++;
+            r++;
+            if(nums[r]==1) count1+=1;
+            if(count1>=mid) return true;
+        }
+        return false;
+    }
+    int longestSubarray(vector<int>& nums) {
+        int l=0,r=nums.size()-1,ret=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(ispossible(nums,mid)){
+                ret=mid;
+                l=mid+1;
+            }
+            else r=mid-1;
+        }
+        return ret;
+    }
+};
